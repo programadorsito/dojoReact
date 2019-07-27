2393f135e08eb3f6c4cdeb629ceaed74617cf8e6
programadorsito <programadorsito@gmail.com>
2019-07-27 10:53:52 -0500

    pokedex

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Poke from './Pokedex/Poke';
+
+function App() {
+  return (
+    <div><Poke id="25" /></div>
+  );
+}
+
+export default App;
